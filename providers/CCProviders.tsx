1e5826b1877346405c5a778a1ba8174041156b23
jkingz <jfkrule@gmail.com>
2025-04-29 00:33:53 +0800

    feat: Add clerk and convex providers

---
+++ providers/CCProviders.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { useAuth } from '@clerk/clerk-react';
+import { ConvexReactClient } from 'convex/react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import React from 'react';
+
+const convex = new ConvexReactClient(
+  process.env.EXPO_PUBLIC_CONVEX_URL! as string,
+  { unsavedChangesWarning: false },
+);
+// Import your Publishable Key
+const PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
+const CCProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
+
+export default CCProviders;
