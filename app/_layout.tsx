59769930f97fc70b9bffa7d94f1a1ebc65959911
jkingz <jfkrule@gmail.com>
2025-04-29 00:36:38 +0800

    feat: Wrap main layout with convex and clerk providers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,17 @@
 import AppLandingLayout from '@/components/AppLandingLayout';
 import '../global.css';
 
-import { ClerkProvider } from '@clerk/clerk-expo';
-import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import CCProviders from '@/providers/CCProviders';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
-// Import your Publishable Key
-const PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key');
-}
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={PUBLISHABLE_KEY}>
+    <CCProviders>
       <SafeAreaProvider>
-        <SafeAreaView className="flex-1 bg-green-500">
+        <SafeAreaView className="flex-1 bg-black">
           <AppLandingLayout />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+    </CCProviders>
   );
 }
