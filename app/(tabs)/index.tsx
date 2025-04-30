8e4f0fc3b2757a6d6bdc93ba6bbd1f761983bb4b
jkingz <jfkrule@gmail.com>
2025-05-01 05:53:00 +0800

    feat(root-layout): Add stories and post section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
+import { Loader } from '@/components/Loader';
+import Posts from '@/components/Posts';
+import Story from '@/components/Story';
+import STORIES from '@/constants/stories';
+import { api } from '@/convex/_generated/api';
 import { styles } from '@/styles/feed.styles';
 import { useAuth } from '@clerk/clerk-expo';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { useRouter } from 'expo-router';
+import { useQuery } from 'convex/react';
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 
 const App = () => {
   const { signOut } = useAuth();
+
+  const posts = useQuery(api.posts.feedPosts);
+  console.log(posts);
+  if (posts === undefined) return <Loader />;
+  if (posts.length === 0) return <Text>No posts yet</Text>;
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -15,8 +26,26 @@ const App = () => {
           <Ionicons name="log-out-outline" size={24} color="white" />
         </TouchableOpacity>
       </View>
+
+      {/* STORIES SECTION */}
+      <FlatList
+        style={styles.storiesContainer}
+        data={STORIES}
+        renderItem={({ item }) => <Story key={item.id} story={item} />}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+
+      {/* POST SECTION */}
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <Posts key={item._id} post={item} />}
+        keyExtractor={(item) => item._id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 60 }}
+      />
     </View>
   );
 };
-
 export default App;
