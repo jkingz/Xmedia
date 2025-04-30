756567229e7b65047f449338c51e0e32de498c18
jkingz <jfkrule@gmail.com>
2025-05-01 02:47:03 +0800

    feat(components): posts components liked functionality

---
+++ components/Posts.tsx
@@ -0,0 +1,107 @@
+import COLORS from '@/constants/theme';
+import { api } from '@/convex/_generated/api';
+import { styles } from '@/styles/feed.styles';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useMutation } from 'convex/react';
+import { Image } from 'expo-image';
+import { Link } from 'expo-router';
+import { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+// post type
+type PostProps = {
+  post: {
+    _id: Id<"posts">,
+    imageUrl?: string,
+    caption?: string,
+    likes: number,
+    comments: number,
+    _creationTime: number,
+    isLiked: boolean,
+    isBookmarked: boolean,
+    author: {
+      _id: string
+      username: string,
+      image: string,
+    }
+  }
+};
+
+const Posts = ({ post }: PostProps) => {
+  const [isLiked, setIsLiked] = useState(post.isLiked)
+  const [likesCount, setLikesCount] = useState(post.likes)
+
+  const toggleLike = useMutation(api.posts.toggleLiked)
+
+  const handleLike = async () => {
+    try {
+      // TODO: Implement like functionality
+      const newIsLiked = await toggleLike({ postId: post._id })
+      setIsLiked(newIsLiked)
+      setLikesCount((prev) => newIsLiked ? prev + 1 : prev - 1)
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  return (
+    <View style={styles.post}>
+      <View style={styles.postHeader}>
+        <Link href={'/(tabs)/notifications'}>
+          <TouchableOpacity style={styles.postHeaderLeft}>
+            <Image
+              source={post.author.image}
+              style={styles.postAvatar}
+              contentFit='cover'
+              transition={200}
+              cachePolicy={'memory-disk'}
+            />
+            <Text style={styles.postUsername}>{post.author.username}</Text>
+          </TouchableOpacity>
+        </Link>
+
+        <TouchableOpacity>
+          <Ionicons name="trash-outline" size={24} color={COLORS.primary} />
+        </TouchableOpacity>
+      </View>
+      {/* post image */}
+      <Image
+        source={post.imageUrl}
+        style={styles.postImage}
+        contentFit='cover'
+        transition={200}
+        cachePolicy={'memory-disk'}
+      />
+      {/* POST ACTIONS */}
+      <View style={styles.postActions}>
+        <View style={styles.postActionsLeft}>
+          <TouchableOpacity onPress={() => handleLike()}>
+            <Ionicons name={isLiked ? 'heart' : 'heart-outline'} size={24} color={isLiked ? COLORS.primary : COLORS.white} />
+          </TouchableOpacity>
+          <TouchableOpacity >
+            <Ionicons name="chatbubble-outline" size={22} color={COLORS.white} />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity>
+          <Ionicons name="bookmark-outline" size={24} color={COLORS.white} />
+        </TouchableOpacity>
+      </View>
+      {/* POST INFO */}
+      <View style={styles.postInfo}>
+        <Text style={styles.likesText}>{likesCount > 0 ? `${likesCount.toLocaleString()} likes` : 'Be the first to like this post!'}</Text>
+        {post.caption && (
+          <View style={styles.captionContainer}>
+            <Text style={styles.captionUsername}>{post.author.username}</Text>
+            <Text style={styles.captionText}>{post.caption}</Text>
+          </View>
+        )}
+        <TouchableOpacity>
+          <Text style={styles.commentText}>View comments</Text>
+        </TouchableOpacity>
+        <Text style={styles.timeAgo}>2 minutes ago</Text>
+      </View>
+    </View >
+  );
+};
+
+export default Posts;
