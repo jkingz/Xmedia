db31a15e76b4efc45da15c45e099921b6882d48c
jkingz <jfkrule@gmail.com>
2025-05-01 00:48:01 +0800

    chore(constants): Add dummy stories data

---
+++ constants/stories.ts
@@ -0,0 +1,57 @@
+const STORIES = [
+  {
+    id: '1',
+    username: 'johndoe',
+    avatar:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    image:
+      'https://images.unsplash.com/photo-1682687220063-4742bd7fd538?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    hasStory: false,
+  },
+  {
+    id: '2',
+    username: 'janedoe',
+    avatar:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    image:
+      'https://images.unsplash.com/photo-1682687220208-22d7a2543e88?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    hasStory: false,
+  },
+  {
+    id: '3',
+    username: 'mike_smith',
+    avatar:
+      'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    image:
+      'https://images.unsplash.com/photo-1682695797873-aa4cb6edd613?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    hasStory: true,
+  },
+  {
+    id: '4',
+    username: 'sarah_j',
+    avatar:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    image:
+      'https://images.unsplash.com/photo-1682695796954-bad0d0f59ff1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    hasStory: false,
+  },
+  {
+    id: '5',
+    username: 'alex_tech',
+    avatar:
+      'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    image:
+      'https://images.unsplash.com/photo-1682687220923-c58b9a4592ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    hasStory: false,
+  },
+  {
+    id: '6',
+    username: 'emma_design',
+    avatar:
+      'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    image:
+      'https://images.unsplash.com/photo-1682695794816-7b9da18ed470?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3',
+    hasStory: true,
+  },
+];
+export default STORIES;
