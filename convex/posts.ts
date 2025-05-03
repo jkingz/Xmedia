d6dcda9328973a9d748857115528f770be98212c
jkingz <jfkrule@gmail.com>
2025-05-04 06:08:25 +0800

    feat(convex): Add delete notifications associated with post

--- convex/posts.ts
+++ convex/posts.ts
@@ -173,6 +173,15 @@ export const deletePost = mutation({
       await ctx.db.delete(bookmark._id);
     }
 
+    // delete all notifications
+    const notifications = await ctx.db
+      .query('notifications')
+      .withIndex('by_post', (q) => q.eq('postId', args.postId))
+      .collect();
+    for (const notification of notifications) {
+      await ctx.db.delete(notification._id);
+    }
+
     // delete image
     await ctx.storage.delete(post.storageId);
 
--- convex/schema.ts
+++ convex/schema.ts
@@ -58,7 +58,9 @@ export default defineSchema({
     type: v.union(v.literal('follow'), v.literal('like'), v.literal('comment')),
     postId: v.id('posts'),
     commentId: v.optional(v.id('comments')),
-  }).index('by_receiver', ['receiverId']),
+  })
+    .index('by_receiver', ['receiverId'])
+    .index('by_post', ['postId']),
 
   // Bookmarks table
   bookmarks: defineTable({
